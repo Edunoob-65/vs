9e7ea955d79389b6480eabe0453e5af89b69e1cf
Edunoob-65 <132776256+Edunoob-65@users.noreply.github.com>
2024-07-20 18:13:25 -0600

    Update user.dart

--- lib/modelos/user.dart
+++ lib/modelos/user.dart
@@ -23,47 +23,4 @@ class User {
   }
 }
 
-// modelo/receta.dart
-class Receta {
-  int id;
-  int usuarioId;
-  String titulo;
-  String ingredientes;
-  String pasos;
-  String? rutaImagen;
-  String categoria;
 
-  Receta({
-    required this.id,
-    required this.usuarioId,
-    required this.titulo,
-    required this.ingredientes,
-    required this.pasos,
-    this.rutaImagen,
-    required this.categoria,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'usuarioId': usuarioId,
-      'titulo': titulo,
-      'ingredientes': ingredientes,
-      'pasos': pasos,
-      'rutaImagen': rutaImagen,
-      'categoria': categoria,
-    };
-  }
-
-  factory Receta.fromMap(Map<String, dynamic> map) {
-    return Receta(
-      id: map['id'],
-      usuarioId: map['usuarioId'],
-      titulo: map['titulo'],
-      ingredientes: map['ingredientes'],
-      pasos: map['pasos'],
-      rutaImagen: map['rutaImagen'],
-      categoria: map['categoria'],
-    );
-  }
-}
